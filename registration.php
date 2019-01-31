29c82d05357519651961b676e0b989bfc5634658
ijazalipse <ijaz.ali.sse@gmail.com>
2019-02-01 02:22:37 +0500

    initial commit for icon only.

---
+++ registration.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ */
+
+/**
+ ** @category   Innovadel Technologies Ltd.
+ * @package    Znsol_Wishlist
+ * @subpackage Block
+ ** @copyright  Copyright (c) 2019 Innovadel Technologies (http://www.innovadeltech.com)
+ * @version    ${release.version}
+ * @link       http://www.innovadeltech.com/
+ * @author     Innovadel Technologies <support@innovadeltech.com>
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Znsol_Base',
+    __DIR__
+);
